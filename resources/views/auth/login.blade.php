10f688254f391660b75115f9d6c6216edcc2108a
Matt Robitaille <matthew@robitaille.io>
2016-01-03 17:06:35 -0500

    Styling/html on login form. Start errorbag setup.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,14 @@
 @section('content')
     <div class="container">
         <div class="row row--wide-gutter row--vh row--vc row--hc">
-            <div class="col col-phablet-3">
-                <form class="form login-form" role="form" method="POST" action="{{ url('/login') }}">
+            <div class="col col-phablet-8 col-sm-4">
+                <form class="form login-form" role="form" method="POST" action="{{ url('/auth/login') }}">
                     {!! csrf_field() !!}
 
+                    @include('layouts.partials.errors')
+
                     <div class="form__row row">
-                        <div class="col col-md-6">
+                        <div class="col">
                             <label for="email" class="form__label sr-only">Email</label>
                             <input type="email" id="email" class="form__input" name="email" value="{{ old('email') }}"
                                    placeholder="Email">
@@ -16,7 +18,7 @@
                     </div>
 
                     <div class="form__row row">
-                        <div class="col col-md-6">
+                        <div class="col">
                             <label for="password" class="form__label sr-only">Password</label>
                             <input type="password" id="password" class="form__input" name="password"
                                    placeholder="Password">
@@ -33,12 +35,14 @@
                         </div>
                     </div>
 
-                    <div class="form__row row">
+                    <div class="form__row row row--reverse row--vc">
                         <div class="col col-md-6">
                             <button type="submit" class="form__button button button--block">
                                 <i class="fa fa-btn fa-sign-in"></i>Login
                             </button>
 
+                        </div>
+                        <div class="col col-md-6">
                             <a class="login-form__forgot" href="{{ url('/password/reset') }}">Forgot Your
                                 Password?</a>
                         </div>
