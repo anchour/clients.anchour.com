6f6fe19bef230db384693c886fd306c8d964ca40
Matt Robitaille <matthew@robitaille.io>
2016-01-09 21:47:21 -0500

    Include vueify, compile main.js via browserify.
    Add vue, vue-resource.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import elixir from 'laravel-elixir';
 
+require('laravel-elixir-vueify');
+
 // Include all paths for the plugin
 elixir.config.css.sass.pluginOptions = {
     outputStyle: 'nested', // libsass doesn't support expanded yet
@@ -10,6 +12,7 @@ elixir.config.css.sass.pluginOptions = {
 elixir(function(mix) {
     mix.sass('main.scss')
         .phpUnit()
+        .browserify('main.js')
         .browserSync({
             proxy: 'clients.dev'
         });
