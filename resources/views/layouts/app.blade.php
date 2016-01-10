f329b8bca5afac68c6866ab426323b1c7cc613bf
Matt Robitaille <matthew@robitaille.io>
2016-01-09 21:48:39 -0500

    Remove default wrapping elements. Add #app wrapper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,9 @@
 @include('layouts.partials.head')
 @include('layouts.partials.header')
 
-<div class="container container--xl">
-    <div class="row row--wide-gutter">
-        <div class="col">
-            @include('layouts.partials.errors')
-            @yield('content')
-        </div>
-    </div>
+<div id="app">
+    @include('layouts.partials.errors')
+    @yield('content')
 </div>
 
 @include('layouts.partials.footer')
