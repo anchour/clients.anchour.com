26e499dc587432ebe79381a565496597e3961385
Matt Robitaille <matthew@robitaille.io>
2016-01-18 21:48:49 -0500

    Add images, gulp images piped to public/images

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,5 @@
 import elixir from 'laravel-elixir';
+import gulp from 'gulp';
 
 require('laravel-elixir-vueify');
 
@@ -7,13 +8,22 @@ elixir.config.css.sass.pluginOptions = {
     outputStyle: 'nested', // libsass doesn't support expanded yet
     precision: 10,
     includePaths: ['.']
-};
+}
+
+elixir.extend('images', function() {
+	gulp.task('images', function() {
+		gulp.src('./resources/assets/images/**/*')
+			.pipe(gulp.dest('public/images'));
+		});
+});;
 
 elixir(function(mix) {
     mix.sass('main.scss')
-        .phpUnit()
-        .browserify('main.js')
+        .images()
         .browserSync({
             proxy: 'clients.dev'
         });
+
+    mix.browserify('main.js')
 });
+
