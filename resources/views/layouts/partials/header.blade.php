a2346390b5797e87fc4dc7e07fe72cb0d94ffa31
Matt Robitaille <matthew@robitaille.io>
2016-01-18 21:50:05 -0500

    Header styling - navigation line height, header logo image

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,8 @@
 <header class="site-header">
     <h1 class="site-header__logo">
-        <a href="{{ url('/') }}">Anchour</a>
+        <a href="{{ url('/') }}">
+        	<img src="/images/logo.svg" alt="Anchour">
+        </a>
     </h1>
     @include('layouts.partials.nav')
 </header>
