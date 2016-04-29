284882aa64b074dae74ee48204d8a627b3b68f0b
Matt Robitaille <matthew@robitaille.io>
2016-04-29 11:35:56 -0400

    Cleanup/extract out the submit form process
    The submit form process will be used in other tests, namely the user model creation test.

--- tests/feature/CustomerCanCreateAccountTest.php
+++ tests/feature/CustomerCanCreateAccountTest.php
@@ -2,33 +2,51 @@
 
 class CustomerCanCreateAccountTest extends TestCase {
 
-    /** @test */
-    public function form_shows_on_customer_new_page()
+    /**
+     * Submit the new customer form.
+     *
+     * @param $details
+     * @return $this
+     */
+    private function submitNewCustomerForm($details)
     {
-        // Form details
-        $name      = 'Customer Name';
-        $email     = 'customer@email.com';
-        $phone     = '2075555555';
-        $ccNumber  = '4242424242424242';
-        $cvc       = '424';
-        $ccExpMonth = date('m');
-        $ccExpYear = date('Y') + 2;
-
         $this->visit('/customer/new')
             ->see('Create your account')
-            ->type($name, 'name')
-            ->type($email, 'email')
-            ->type($phone, 'phone')
-            ->type($ccNumber, 'cc-number')
-            ->type($cvc, 'cc-cvc')
-            ->select($ccExpMonth, 'cc-exp-month')
-            ->select($ccExpYear, 'cc-exp-year')
+            ->type($details['name'], 'name')
+            ->type($details['email'], 'email')
+            ->type($details['phone'], 'phone')
+            ->type($details['ccNumber'], 'cc-number')
+            ->type($details['cvc'], 'cc-cvc')
+            ->select($details['ccExpMonth'], 'cc-exp-month')
+            ->select($details['ccExpYear'], 'cc-exp-year')
             ->submitForm('Submit');
 
-        $this->seePageIs('/customer/details')
+        return $this;
+    }
+
+    /**
+     * Check that the details page shows the customer's details
+     * that were submitted to the database.
+     *
+     * @test
+     */
+    public function form_shows_on_customer_new_page()
+    {
+        $details = [
+            'name'       => 'Customer Name',
+            'email'      => 'customer@email.com',
+            'phone'      => '2075555555',
+            'ccNumber'   => '4242424242424242',
+            'cvc'        => '424',
+            'ccExpMonth' => date('m'),
+            'ccExpYear'  => date('Y') + 2,
+        ];
+
+        $this->submitNewCustomerForm($details)
+            ->seePageIs('/customer/details')
             ->see('Thanks!')
-            ->see($name)
-            ->see($email)
-            ->see($phone);
+            ->see($details['name'])
+            ->see($details['email'])
+            ->see($details['phone']);
     }
 }
